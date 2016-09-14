4b4934c2f1b9cdb0a2ce7a8713f234acb485bfa0
Carlos A. Blanco <pctoyscarlosb@outlook.com>
2016-09-14 13:55:28 -0400

    Add files via upload

---
+++ [Ready] ReaisToDollars.java
@@ -0,0 +1,23 @@
+import.java.util.*;
+
+public class ReaisToDollars {
+
+      public static void main(String[] args {
+      
+      Scanner console = new Scanner(System.in);
+      final double RATE = 3.27;
+      
+      double reais;
+      double dollars;
+      
+      System.out.printl("Please enter a number of Reais");
+      dollars = console.nextDouble();
+      
+      dollars = reais * RATE;
+      
+      System.out.println("The total amount in US Dollars is" + dollars);
+      
+      
+      }
+      
+ }
